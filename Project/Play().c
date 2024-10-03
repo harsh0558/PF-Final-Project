440a30973d6ffff6cdd8c02473ad9cefb2b076d2
Harsh <hmaghnani6@gmail.com>
2024-10-03 23:27:01 +0500

    Update Play().c

--- Project/Play().c
+++ Project/Play().c
@@ -5,23 +5,24 @@ void play(){
     char answer[20];
     printf("The main word is: %s\n",WordArray[0]);
     do{
-        printf("enter your guess(type No to stop): ");
-        scanf("%s",answer);
+        printf("enter your guess(type 'No' to stop): ");
+        scanf(" %s",answer);
+        int found = 0;
         if(strcmp(answer, "No") == 0){
-            printf("You guessed %d answers correctly",correct);
+            printf("You guessed %d answers correctly\n",correct);
         }else{
-            for(i=1;i<=NumberWords-1;i++){
+            for(i=1;i<=NumberWords;i++){
                 if(strcmp(WordArray[i], answer) == 0){
-                    printf("you gussed correctly :)\n");
                     WordArray[i][0] = '\0';
                     correct ++;
-                    break;
-                }else{
-                    printf("oh no!!! this is not one of the answers :(\n");
+                    printf("you guessed the word correctly :)\n");
+                    found = 1;
                     break;
                 }
             }
-        }
+        }   if(found == 0 && strcmp(answer,"No") != 0){
+            printf("your guess is incorrect :(\n");
+            }
     } while(strcmp(answer,"No") != 0 );
     
 }
